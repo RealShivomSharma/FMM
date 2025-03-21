17080bcac9123b79c82d9a8c53c5daf0215344de
Shivom Sharma <shars119@mcmaster.ca>
2025-03-20 22:50:37 -0400

    Removed main code

--- py/main.py
+++ py/main.py
@@ -1,32 +1,3 @@
-import numpy as np
-
-
-# summation formula for a given function
-
-
-# for a phi where phi is = (x[i] - y[j]**2)
-
-
-# x and y are just a list of points
-
-
-def summation(phi: np.array, q: np.array):
-
-    N = len(phi[0])
-
-    u = np.zeros(N, 1)
-
-    for j in range(1, N):
-        alpha = alpha + q[j]
-        beta = alpha + q[j] * x[j]
-        gamma = gamma + q[j] * (x[j] ** 2)
-
-    for i in range(1, N):
-        u[i] = alpha * y[i] ** 2 - 2 * beta * y[i] + gamma
-
-    return u
-
-
 def main():
 
     print("Hello from fmm!")
