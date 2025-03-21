5e48d13dd536371c4efa1146077b13a63f59e88f
Shivom Sharma <shars119@mcmaster.ca>
2025-03-21 02:32:39 -0400

    Converted to pytest and added initial test for far field approximation, seems wrong, need to review

--- py/tests/test_tree.py
+++ py/tests/test_tree.py
@@ -1,42 +1,59 @@
 import numpy as np
 import sys
-import unittest
+import pytest
 
 sys.path.append("..")
 import tree as t
 
 
-class TestMultipoleWeights(unittest.TestCase):
+def test_multipole_weights():
 
-    def test_multipole_weights(self):
+    # Create Tree
+    T = (0, 1)
+    k = 20
+    p = 2
+    tree = t.construct_tree(T, k)
 
-        # Create Tree
-        T = (0, 1)
-        k = 3
-        p = 2
-        tree = t.construct_tree(T, k)
+    # initialize sources and charges
+    sources = np.array([0.1, 0.2, 0.6, 0.9])
+    charges = np.array([1.0, -1.0, 1.0, -1.0])
 
-        # initialize sources and charges
-        sources = np.array([0.1, 0.2, 0.6, 0.9])
-        charges = np.array([1.0, -1.0, 1.0, -1.0])
+    # Compute weights iterative using level order
+    t.compute_weights_iterative(tree.root, sources, charges, p)
 
-        # Compute weights iterative using level order
-        t.compute_weights_iterative(tree.root, sources, charges, p)
+    # Brute-force reference computation
 
-        # Brute-force reference computation
+    # Expected weights for node 1
+    expected_weights = np.array(
+        [
+            np.sum(charges),
+            np.sum(charges * (sources - tree.root.M)),
+            np.sum(charges * (sources - tree.root.M) ** 2),
+        ]
+    )
 
-        # Expected weights for node 1
-        expected_weights = np.array(
-            [
-                np.sum(charges),
-                np.sum(charges * (sources - tree.root.M)),
-                np.sum(charges * (sources - tree.root.M) ** 2),
-            ]
-        )
-
-        self.assertTrue(np.allclose(tree.root.weights, expected_weights, atol=1e-6))
+    assert np.allclose(tree.root.weights, expected_weights, atol=1e-6)
 
 
-if __name__ == "__main__":
+# TODO: Need to most likely fix this test/review FFA
+def test_far_field_potential():
+    T = (0, 1)
+    k = 15
+    p = 2
+    delta = 2.0
+    tree = t.construct_tree(T, k)
 
-    unittest.main()
+    sources = np.array([0.1, 0.2, 0.6, 0.9])
+    charges = np.array([1.0, -1.0, 1.0, -1.0])
+
+    t.compute_weights_iterative(tree.root, sources, charges, p)
+
+    targets = np.array([0.05, 0.25, 0.55, 0.85])
+    far_field_potential = t.far_field_approximation(tree, delta, targets, p)
+
+    # Brute-force direct computation
+    direct_potential = np.array(
+        [np.sum(charges / np.abs(target - sources)) for target in targets]
+    )
+
+    assert np.allclose(far_field_potential, direct_potential, atol=1e-3)
