0993956cf833a6bae31c572f3d9a2a1143ed8626
Shivom Sharma <shars119@mcmaster.ca>
2025-03-21 23:34:16 -0400

    Implemented initial construct_tree() method for HMatrix

--- py/HMatrix.py
+++ py/HMatrix.py
@@ -37,3 +37,47 @@ class HMatrix:
 
         # Construct tree from root matrix
         self.construct_tree(self.root, matrix)
+
+    def construct_tree(self, node: MatrixNode, sub_matrix: np.array):
+
+        r_start, r_end = node.row_range
+        c_start, c_end = node.col_range
+
+        # If the size is too small end recursion
+
+        if node.shape[0] <= self.min_size or node.shape[1] <= self.min_size:
+            node.is_leaf = True  # no longer subdividing
+            U, s, Vh = svd(sub_matrix, full_matrices=False)
+            rank = np.sum(s > self.tol)
+
+            if rank <= self.max_rank:
+                # If rank is low enough then we perform and store low rank approximation
+                node.data = (U[:, :rank] * s[:rank], Vh[:rank, :])
+            else:
+                # Otherwise keep dense structure
+                node.data = sub_matrix.copy()
+
+            return
+
+        r_mid = (r_start + r_end) // 2
+        c_mid = (c_start + c_end) // 2
+
+        partitions = [
+            (r_start, r_mid, c_start, c_mid),  # Top left
+            (r_start, r_mid, c_mid, c_end),  # Top Right
+            (r_mid, r_end, c_start, c_mid),  # Bottom Left
+            (r_mid, r_end, c_mid, c_end),  # Bottom Right
+        ]
+        for r_start, r_end, c_start, c_end in partitions:
+            child = MatrixNode(r_start, r_end, c_start, c_end)
+            node.children.append(child)
+            self.construct_tree(child, self.matrix[r_start:r_end, c_start, c_end])
+
+
+if __name__ == "__main__":
+
+    A = np.random.rand(4, 4)
+
+    test_matrix = HMatrix(A)
+
+    print(test_matrix.root.data)
